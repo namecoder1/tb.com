0233ef1a00c4dcec98fb612e03e150c3ff1094e0
namecoder1 <bartolomei.private@gmail.com>
2024-09-26 21:17:15 +0200

    Bug fix for useSearchParams()

--- app/(frontend)/search/page.jsx
+++ app/(frontend)/search/page.jsx
@@ -2,8 +2,7 @@
 
 import Spinner from '@/components/ui/Spinner'
 import PostCard from '@/components/ui/PostCard';
-import Image from 'next/image';
-import Link from 'next/link';
+import { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
@@ -33,35 +32,37 @@ export default function SearchResults() {
 	console.log(results)
 
   return (
-    <div className='my-20 mx-10 lg:mx-20 2xl:mx-32 '>
-      <h1 className='text-3xl sm:text-4xl md:text-5xl font-semibold mb-10'>Risultati per: "{query}"</h1>
-      {loading ? (
-        <div className='h-[100vh] flex flex-col items-center justify-center'>
-          <Spinner />
-        </div>
-      ) : results.length > 0 ? (
-        <ul className='flex flex-col md:grid md:grid-cols-2 xl:grid xl:grid-cols-3 my-20 gap-10'>
-          {results.map((result) => (
-            <li key={result.id}>
-							<PostCard 
-								slug={result.slug} 
-								title={result.title}
-								image={result.image}
-								altText={result.altText} 
-								description={result.description}
-								id={result.id}
-								categories={result.categories}
-								readTime={result.readTime}
-								date={result._updatedAt}
-							/>            
-						</li>
-          ))}
-        </ul>
-      ) : (
-        <div className='h-[100vh] flex flex-col items-center justify-center'>
-          <p className='text-xl md:text-2xl font-medium'>Nessun risultato trovato</p>
-        </div>
-      )}
-    </div>
+    <Suspense>
+      <div className='my-20 mx-10 lg:mx-20 2xl:mx-32 '>
+        <h1 className='text-3xl sm:text-4xl md:text-5xl font-semibold mb-10'>Risultati per: "{query}"</h1>
+        {loading ? (
+          <div className='h-[100vh] flex flex-col items-center justify-center'>
+            <Spinner />
+          </div>
+        ) : results.length > 0 ? (
+          <ul className='flex flex-col md:grid md:grid-cols-2 xl:grid xl:grid-cols-3 my-20 gap-10'>
+            {results.map((result) => (
+              <li key={result.id}>
+                <PostCard 
+                  slug={result.slug} 
+                  title={result.title}
+                  image={result.image}
+                  altText={result.altText} 
+                  description={result.description}
+                  id={result.id}
+                  categories={result.categories}
+                  readTime={result.readTime}
+                  date={result._updatedAt}
+                />            
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <div className='h-[100vh] flex flex-col items-center justify-center'>
+            <p className='text-xl md:text-2xl font-medium'>Nessun risultato trovato</p>
+          </div>
+        )}
+      </div>
+    </Suspense>
   );
 }
